0946b3c3885a4bfafd31046b41188f617ba282f4
Alon Soffer <alon.soffer@mail.huji.ac.il>
2021-04-06 12:22:33 +0300

    calculator functionality mostly done. some fetures yet to be verified. basic +/-/= work

--- app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
+++ app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
@@ -6,20 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-  @VisibleForTesting
-  public SimpleCalculator calculator;
+    @VisibleForTesting
+    public SimpleCalculator calculator;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    if (calculator == null) {
-      calculator = new SimpleCalculatorImpl();
-    }
+        if (calculator == null) {
+            calculator = new SimpleCalculatorImpl();
+        }
 
     /*
     TODO:
@@ -27,17 +31,67 @@ public class MainActivity extends AppCompatActivity {
     - initial update main text-view based on calculator's output
     - set click listeners on all buttons to operate on the calculator and refresh main text-view
      */
-  }
+        TextView buttonEquals = findViewById(R.id.buttonEquals);
+        TextView buttonPlus = findViewById(R.id.buttonPlus);
+        TextView buttonMinus = findViewById(R.id.buttonMinus);
+        TextView buttonClear = findViewById(R.id.buttonClear);
+        TextView button0 = findViewById(R.id.button0);
+        TextView button2 = findViewById(R.id.button2);
+        TextView button5 = findViewById(R.id.button5);
+        TextView button8 = findViewById(R.id.button8);
+        TextView button1 = findViewById(R.id.button1);
+        TextView button4 = findViewById(R.id.button4);
+        TextView button7 = findViewById(R.id.button7);
+        ImageView buttonBackSpace = findViewById(R.id.backSpaceImage);
+        TextView button3 = findViewById(R.id.button3);
+        TextView button6 = findViewById(R.id.button6);
+        TextView button9 = findViewById(R.id.button9);
+        TextView calcOutput = findViewById(R.id.textViewCalculatorOutput);
 
-  @Override
-  protected void onSaveInstanceState(@NonNull Bundle outState) {
-    super.onSaveInstanceState(outState);
-    // todo: save calculator state into the bundle
-  }
+        calcOutput.setText(calculator.output());
+        TextView[] digitButtons =  {button0, button1, button2, button3, button4, button5, button6, button7, button8, button9};
+        for (int i = 0; i < 10; i++)
+        {
+            int digit = i;
+            digitButtons[i].setOnClickListener(v -> {
+                calculator.insertDigit(digit);
+                calcOutput.setText(calculator.output());
+            });
+        }
+        buttonPlus.setOnClickListener(v -> {
+            calculator.insertPlus();
+            calcOutput.setText(calculator.output());
+        });
+        buttonMinus.setOnClickListener(v -> {
+            calculator.insertMinus();
+            calcOutput.setText(calculator.output());
+        });
+        buttonBackSpace.setOnClickListener(v -> {
+            calculator.deleteLast();
+            calcOutput.setText(calculator.output());
+        });
+        buttonClear.setOnClickListener(v -> {
+            calculator.clear();
+            calcOutput.setText(calculator.output());
+        });
+        buttonEquals.setOnClickListener(v -> {
+            calculator.insertEquals();
+            calcOutput.setText(calculator.output());
+        });
 
-  @Override
-  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-    super.onRestoreInstanceState(savedInstanceState);
-    // todo: restore calculator state from the bundle, refresh main text-view from calculator's output
-  }
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // todo: save calculator state into the bundle
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // todo: restore calculator state from the bundle, refresh main text-view from calculator's output
+    }
 }
--- app/src/main/java/android/exercise/mini/calculator/app/SimpleCalculatorImpl.java
+++ app/src/main/java/android/exercise/mini/calculator/app/SimpleCalculatorImpl.java
@@ -1,69 +1,181 @@
 package android.exercise.mini.calculator.app;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
+class CalculatorTokenizer
+{
+    private final ArrayList<String> inputs;
+    private final HashSet<String> operations;
+    private int pointer = 0;
+
+    public CalculatorTokenizer(ArrayList<String> inputs, HashSet<String> operations)
+    {
+        this.inputs = inputs;
+        this.operations = operations;
+    }
+
+    public String getToken()
+    {
+        if (this.operations.contains(this.inputs.get(this.pointer)))
+        {
+            String ret = this.inputs.get(this.pointer);
+            this.pointer++;
+            return ret;
+        }
+        // not an operation, read until see an operation
+        StringBuilder token = new StringBuilder();
+        while (!this.operations.contains(this.inputs.get(this.pointer)))
+        {
+            token.append(this.inputs.get(this.pointer));
+            this.pointer++;
+        }
+        return token.toString();
+    }
+}
 public class SimpleCalculatorImpl implements SimpleCalculator {
 
-  // todo: add fields as needed
+    // todo: add fields as needed
+    private ArrayList<String> inputs = new ArrayList<String>();
+    private boolean equals = false;
+    private static final HashSet<String> operations = new HashSet<String>(Arrays.asList("+","-","="));
 
-  @Override
-  public String output() {
-    // todo: return output based on the current state
-    return "implement me please";
-  }
-
-  @Override
-  public void insertDigit(int digit) {
-    // todo: insert a digit
-  }
-
-  @Override
-  public void insertPlus() {
-    // todo: insert a plus
-  }
-
-  @Override
-  public void insertMinus() {
-    // todo: insert a minus
-  }
-
-  @Override
-  public void insertEquals() {
-    // todo: calculate the equation. after calling `insertEquals()`, the output should be the result
-    //  e.g. given input "14+3", calling `insertEquals()`, and calling `output()`, output should be "17"
-  }
-
-  @Override
-  public void deleteLast() {
-    // todo: delete the last input (digit, plus or minus)
-    //  e.g.
-    //  if input was "12+3" and called `deleteLast()`, then delete the "3"
-    //  if input was "12+" and called `deleteLast()`, then delete the "+"
-    //  if no input was given, then there is nothing to do here
-  }
-
-  @Override
-  public void clear() {
-    // todo: clear everything (same as no-input was never given)
-  }
-
-  @Override
-  public Serializable saveState() {
-    CalculatorState state = new CalculatorState();
-    // todo: insert all data to the state, so in the future we can load from this state
-    return state;
-  }
-
-  @Override
-  public void loadState(Serializable prevState) {
-    if (!(prevState instanceof CalculatorState)) {
-      return; // ignore
+    private String calculate()
+    {
+        System.out.println("calculating");
+        String operation = "+";
+        int res = 0;    //TODO: is int enough?
+        CalculatorTokenizer tokenizer = new CalculatorTokenizer(this.inputs, SimpleCalculatorImpl.operations);
+        String token = tokenizer.getToken();
+        while (!token.equals("="))
+        {
+            if (SimpleCalculatorImpl.operations.contains(token))
+            {
+                operation = token;
+            }
+            else
+            {
+                int intVal = Integer.parseInt(token);
+                switch (operation)
+                {
+                    case "+":
+                        res += intVal;
+                        break;
+                    case "-":
+                        res -= intVal;
+                        break;
+                }
+            }
+            token = tokenizer.getToken();
+        }
+        this.clear();
+        System.out.println("returning: " + Integer.toString(res));
+        return Integer.toString(res);
     }
-    CalculatorState casted = (CalculatorState) prevState;
-    // todo: use the CalculatorState to load
-  }
 
-  private static class CalculatorState implements Serializable {
+    @Override
+    public String output()
+    {
+        // todo: return output based on the current state
+        if (this.equals)
+        {
+            System.out.println("printing equals to view");
+            this.equals = false;
+            return this.calculate();
+        }
+        else
+        {
+            StringBuilder out_p = new StringBuilder();
+            for (String s: this.inputs)
+            {
+                out_p.append(s);
+            }
+            return out_p.toString();
+        }
+    }
+
+    private void insert_something(String toInsert)
+    {
+        this.inputs.add(toInsert);
+    }
+
+    @Override
+    public void insertDigit(int digit) {
+        // todo: insert a digit
+//    this.inputs.add(Integer.toString(digit));
+        this.insert_something(Integer.toString(digit));
+    }
+
+    private void insertOperation(String op)
+    {
+        if (this.inputs.size() > 0 && SimpleCalculatorImpl.operations.contains(this.inputs.get(this.inputs.size()-1)))
+        {
+            this.deleteLast();
+        }
+        this.insert_something(op);
+    }
+    @Override
+    public void insertPlus() {
+        // todo: insert a plus
+//        this.insert_something("+");
+        this.insertOperation("+");
+    }
+
+    @Override
+    public void insertMinus() {
+        // todo: insert a minus
+//        this.insert_something("-");
+        this.insertOperation("-");
+    }
+
+    @Override
+    public void insertEquals()
+    {
+        // todo: calculate the equation. after calling `insertEquals()`, the output should be the result
+        //  e.g. given input "14+3", calling `insertEquals()`, and calling `output()`, output should be "17"
+
+//        TODO: what happens if last input is operation (+/-)?
+        insert_something("=");
+        equals = true;
+        System.out.println("equals pressed");
+    }
+
+    @Override
+    public void deleteLast()
+    {
+        // todo: delete the last input (digit, plus or minus)
+        //  e.g.
+        //  if input was "12+3" and called `deleteLast()`, then delete the "3"
+        //  if input was "12+" and called `deleteLast()`, then delete the "+"
+        //  if no input was given, then there is nothing to do here
+        inputs.remove(inputs.size()-1);
+    }
+
+    @Override
+    public void clear() {
+        // todo: clear everything (same as no-input was never given)
+        inputs = new ArrayList<String>();
+    }
+
+    @Override
+    public Serializable saveState() {
+        CalculatorState state = new CalculatorState();
+        // todo: insert all data to the state, so in the future we can load from this state
+        return state;
+    }
+
+    @Override
+    public void loadState(Serializable prevState) {
+        if (!(prevState instanceof CalculatorState)) {
+            return; // ignore
+        }
+        CalculatorState casted = (CalculatorState) prevState;
+        // todo: use the CalculatorState to load
+    }
+
+    private static class CalculatorState implements Serializable {
     /*
     TODO: add fields to this class that will store the calculator state
     all fields must only be from the types:
@@ -72,5 +184,5 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
     - ArrayList<> where the type is a primitive or a String
     - HashMap<> where the types are primitives or a String
      */
-  }
+    }
 }
